1b24f7fa938ab885dd8275f10066cd7e6a1e3e15
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-10-10 15:32:55 +0900

    init

---
+++ src/Groth16Verifier.sol
@@ -0,0 +1,198 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+    Copyright 2021 0KIMS association.
+
+    This file is generated with [snarkJS](https://github.com/iden3/snarkjs).
+
+    snarkJS is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snarkJS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Groth16Verifier {
+    // Scalar field size
+    uint256 constant r    = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    // Base field size
+    uint256 constant q   = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+    // Verification Key data
+    uint256 constant alphax  = 18218212254513489393227391265584283766666865803421596704873626382188992217224;
+    uint256 constant alphay  = 14125380554379765159195252431632605300972870314861139725267198112244622893287;
+    uint256 constant betax1  = 544923540962348037148134782760180659573139853834694199532161888014254914561;
+    uint256 constant betax2  = 17426637784740211035711704161198373765418416277982725790392018690291173311125;
+    uint256 constant betay1  = 19667755638807254816766062644661173236564424390495970266421997580868061835303;
+    uint256 constant betay2  = 8765334826481019138627084389843339286975981348493952006771755782032870959985;
+    uint256 constant gammax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 constant gammax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 constant gammay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 constant gammay2 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;
+    uint256 constant deltax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 constant deltax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 constant deltay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 constant deltay2 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;
+
+    
+    uint256 constant IC0x = 18302578470256876791172965421161435126511551927872329376308090826134320551073;
+    uint256 constant IC0y = 19522615991329907481152899345219137123574572892910103174851534164220091852505;
+    
+    uint256 constant IC1x = 21082770244119661879649089626901464446066139107722299572581816731535450648067;
+    uint256 constant IC1y = 16113521986820213617518977344069364887048466637650271403010090961425521679492;
+    
+    uint256 constant IC2x = 20823154644463298385489165324245796673557302073375054780212579351709199982935;
+    uint256 constant IC2y = 13752714205730390640328253046795485658145759790961924022119710134095911085968;
+    
+    uint256 constant IC3x = 9380314440113840563594658581554025155172777543553564600078127412093965643916;
+    uint256 constant IC3y = 17761095896786080324227737130005251815731487482873869502804847431926818939305;
+    
+    uint256 constant IC4x = 5861666701161139888182821437493350800522278968376474694312887095396486181502;
+    uint256 constant IC4y = 14243208620595056332557476307325193721542422238409368792942591013175871521399;
+    
+    uint256 constant IC5x = 20399455037661427830258097867026901392624054148362017519305983349637624278046;
+    uint256 constant IC5y = 8905361699007478643196173185631856020270035381843714683887728431249900892867;
+    
+ 
+    // Memory data
+    uint16 constant pVk = 0;
+    uint16 constant pPairing = 128;
+
+    uint16 constant pLastMem = 896;
+
+    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[5] calldata _pubSignals) public view returns (bool) {
+        assembly {
+            function checkField(v) {
+                if iszero(lt(v, r)) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+            
+            // G1 function to multiply a G1 value(x,y) to value in an address
+            function g1_mulAccC(pR, x, y, s) {
+                let success
+                let mIn := mload(0x40)
+                mstore(mIn, x)
+                mstore(add(mIn, 32), y)
+                mstore(add(mIn, 64), s)
+
+                success := staticcall(sub(gas(), 2000), 7, mIn, 96, mIn, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+
+                mstore(add(mIn, 64), mload(pR))
+                mstore(add(mIn, 96), mload(add(pR, 32)))
+
+                success := staticcall(sub(gas(), 2000), 6, mIn, 128, pR, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+
+            function checkPairing(pA, pB, pC, pubSignals, pMem) -> isOk {
+                let _pPairing := add(pMem, pPairing)
+                let _pVk := add(pMem, pVk)
+
+                mstore(_pVk, IC0x)
+                mstore(add(_pVk, 32), IC0y)
+
+                // Compute the linear combination vk_x
+                
+                g1_mulAccC(_pVk, IC1x, IC1y, calldataload(add(pubSignals, 0)))
+                
+                g1_mulAccC(_pVk, IC2x, IC2y, calldataload(add(pubSignals, 32)))
+                
+                g1_mulAccC(_pVk, IC3x, IC3y, calldataload(add(pubSignals, 64)))
+                
+                g1_mulAccC(_pVk, IC4x, IC4y, calldataload(add(pubSignals, 96)))
+                
+                g1_mulAccC(_pVk, IC5x, IC5y, calldataload(add(pubSignals, 128)))
+                
+
+                // -A
+                mstore(_pPairing, calldataload(pA))
+                mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
+
+                // B
+                mstore(add(_pPairing, 64), calldataload(pB))
+                mstore(add(_pPairing, 96), calldataload(add(pB, 32)))
+                mstore(add(_pPairing, 128), calldataload(add(pB, 64)))
+                mstore(add(_pPairing, 160), calldataload(add(pB, 96)))
+
+                // alpha1
+                mstore(add(_pPairing, 192), alphax)
+                mstore(add(_pPairing, 224), alphay)
+
+                // beta2
+                mstore(add(_pPairing, 256), betax1)
+                mstore(add(_pPairing, 288), betax2)
+                mstore(add(_pPairing, 320), betay1)
+                mstore(add(_pPairing, 352), betay2)
+
+                // vk_x
+                mstore(add(_pPairing, 384), mload(add(pMem, pVk)))
+                mstore(add(_pPairing, 416), mload(add(pMem, add(pVk, 32))))
+
+
+                // gamma2
+                mstore(add(_pPairing, 448), gammax1)
+                mstore(add(_pPairing, 480), gammax2)
+                mstore(add(_pPairing, 512), gammay1)
+                mstore(add(_pPairing, 544), gammay2)
+
+                // C
+                mstore(add(_pPairing, 576), calldataload(pC))
+                mstore(add(_pPairing, 608), calldataload(add(pC, 32)))
+
+                // delta2
+                mstore(add(_pPairing, 640), deltax1)
+                mstore(add(_pPairing, 672), deltax2)
+                mstore(add(_pPairing, 704), deltay1)
+                mstore(add(_pPairing, 736), deltay2)
+
+
+                let success := staticcall(sub(gas(), 2000), 8, _pPairing, 768, _pPairing, 0x20)
+
+                isOk := and(success, mload(_pPairing))
+            }
+
+            let pMem := mload(0x40)
+            mstore(0x40, add(pMem, pLastMem))
+
+            // Validate that all evaluations ∈ F
+            
+            checkField(calldataload(add(_pubSignals, 0)))
+            
+            checkField(calldataload(add(_pubSignals, 32)))
+            
+            checkField(calldataload(add(_pubSignals, 64)))
+            
+            checkField(calldataload(add(_pubSignals, 96)))
+            
+            checkField(calldataload(add(_pubSignals, 128)))
+            
+            checkField(calldataload(add(_pubSignals, 160)))
+            
+
+            // Validate all evaluations
+            let isValid := checkPairing(_pA, _pB, _pC, _pubSignals, pMem)
+
+            mstore(0, isValid)
+             return(0, 0x20)
+         }
+     }
+ }
---
+++ src/sbt.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./Groth16Verifier.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CNUNFT is ERC721URIStorage, Ownable {
+    Groth16Verifier public verifier;
+    uint256 public nextTokenId = 1;
+
+    // 이미 발급받았는지 체크하는 매핑
+    mapping(address => bool) public hasMinted;
+
+    event NFTMinted(address indexed recipient, uint256 tokenId);
+
+    // 수정된 생성자, Ownable의 생성자에 msg.sender 전달
+    constructor(address _verifierAddress) ERC721("CNU NFT", "CNU") Ownable(msg.sender) {
+        verifier = Groth16Verifier(_verifierAddress);
+    }
+
+    // NFT 발급 함수 (zk-SNARK 프루프 검증을 통과해야 발급 가능)
+    function mintNFT(
+        uint[2] calldata _pA, 
+        uint[2][2] calldata _pB, 
+        uint[2] calldata _pC, 
+        uint[5] calldata _pubSignals, 
+        string memory tokenURI
+    ) public {
+        require(!hasMinted[msg.sender], "NFT already minted");
+
+        // zk-SNARK proof 검증
+        require(verifier.verifyProof(_pA, _pB, _pC, _pubSignals), "Invalid proof");
+
+        // NFT 발급 및 메타데이터 설정
+        uint256 tokenId = nextTokenId;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);  // 메타데이터 URI 설정
+
+        hasMinted[msg.sender] = true;
+        nextTokenId++;
+
+        emit NFTMinted(msg.sender, tokenId);
+    }
+}
